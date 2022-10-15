9a40842ebc3f13cf7930412f66174852b713a3a4
Janalisa Waugh <iamjanalisa@gmail.com>
2022-10-14 23:31:51 -0500

    Board is now showing, did exercises 2 and 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,48 @@
-//"use strict";
+"use strict";
 document.addEventListener("DOMContentLoaded", function(){
 
+//variable declarations
 const board = document.querySelector("#board");
-//console.log(board);
-const cells = board.querySelectorAll("div");
-
+const cells = board.querySelectorAll("div");//add board and cells
+//cells. //Exercise 1
+const status = document.querySelector("#status");
+let controls = document.querySelector(".controls");
 const restart = document.getElementById("button");
+let playerX = 'X';
 
-cells.classList.add("square");
-//console.log(board.cells);
+let stateTracker = [];
+let state = true;
+
+//function for Exercise 2
+cells.forEach(function(tile, index){
+    tile.classList.add("square");//add css square class to cells
+    //Exercise 2
+    tile.addEventListener("click", function(e){
+        if(state==true && tile.innerHTML==""){
+            if (playerX == "X"){
+                tile.classList.add("X");
+            }
+            else{
+                tile.classList.add("O");
+            }
+            tile.innerHTML = playerX;
+            stateTracker[index]= playerX;
+
+        
+        }
+    })
+
+
+
+    //Exercise 3
+    tile.addEventListener("mouseover",() =>{ 
+        tile.style.cursor = "pointer";
+        tile.classList.add("hover");
+    })
+    tile.addEventListener("mouseout", () =>{
+        tile.classList.remove("hover");
+    })
+});
 
 /*const winningPlays = [
     [0, 1, 2],
